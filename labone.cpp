cafc563d939f39e9022d32577b2aa8aec8653f2a
Surya-driod <aradhanasingh0505@gmail.com>
2025-02-07 00:00:45 +0530

    Update labone.cpp

--- labone.cpp
+++ labone.cpp
@@ -1,68 +1,58 @@
 #include <iostream>
+#include <vector>
+// algorithm is a library used to sort the array
+#include <algorithm>
 using namespace std;
-class items
-{
-    public:
-       float w;
-       float prpw; 
-       void ini(float j)
-       {
-           w=10*(j/2);
-           prpw=j/2;
-       }
+
+// Structure to represent an item with weight and profit per kg
+struct Item {
+    int wt;
+    double ppk; // profit per kg
 };
 
-int main()
-{
-    items x[10];
-    float k=1;
-    for (int i=1;i<11;i++)
-    {
-        
-        x[i-1].ini(k++);
-    }
-    
-    float tw=0;
-    float arr1[10];
-    float arr2[10];
-    
-    for (int i=0;i<10;i++)
-        {
-             tw+=x[i].w; 
-             if (tw<=200)
-             {
-                 arr1[i]=x[i].w;
-                 arr2[i]=arr1[i]/x[i].w;
-             }
-             else
-             {
-                 arr1[i]=x[i].w-200+tw;
-                 arr2[i]=arr1[i]/x[i].w-1;
-                 break;
-             }
+// Comparator function to sort items by profit density (ppk)
+bool compare(Item a, Item b) {
+    return (a.ppk > b.ppk);
+}
+
+// Function to solve the fractional knapsack problem
+void knapsack(int W, vector<Item> items) {
+    // Sort items by profit density
+    sort(items.begin(), items.end(), compare);
+
+    vector<double> amount(items.size(), 0.0); // Amount of each item taken
+    int current_wt = 0; // Current weight of the knapsack
+    double tpr = 0.0; // Total profit in the knapsack
+
+    for (int i = 0; i < items.size(); i++) {
+        if (current_wt + items[i].wt <= W) {
+            // Take the whole item
+            amount[i] = 1.0;
+            current_wt += items[i].wt;
+            tpr += items[i].ppk * items[i].wt;
+        } else {
+            // Take a fraction of the item
+            double fraction = (double)(W - current_wt) / items[i].wt;
+            amount[i] = fraction;
+            current_wt += fraction * items[i].wt;
+            tpr += items[i].ppk * items[i].wt * fraction;
+            break; // Knapsack is full
         }
-    for (int i=0;i<10;i++)
-    {
-        cout << x[i].w <<" @ "<< x[i].prpw << " || ";
-        
     }
-    for (int i=0;i<10;i++)
-    {
-        cout << arr1[i] << " ";
+
+    cout << "Total profit in the knapsack: " << tpr << endl;
+    cout << "Amounts of each item taken: ";
+    for (double a : amount) {
+        cout << a << " ";
     }
-    cout<<endl;
-    float profit=0;
-    for (int z=0;z<10;z++)
-    {
-        profit=profit+arr1[z]*x[z].prpw;
-    }
-    
-    cout<< "total profit is " << profit<<endl;
-    cout<<"factional distribution of items : " << endl;
-    for (int i=0;i<10;i++)
-    {
-        
-        cout<< arr2[i] << " ";
-    }
+    cout << endl;
+}
+
+int main() {
+    int W = 500; // Maximum weight of knapsack
+    vector<Item> items = {{100, 20}, {200, 30}, {150, 50}, {250, 40}}; // Items with weights and profit per kg
+
+    knapsack(W, items);
+
     return 0;
 }
